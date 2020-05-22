81e2be3237d849093219430d011713c9391d7cd5
BeanGuohui <401220179@qq.com>
2020-05-22 14:25:12 +0800

    基本功能已经全部完成

--- MusicPlayer/mymusicplayer.cpp
+++ MusicPlayer/mymusicplayer.cpp
@@ -8,6 +8,8 @@
 #include <QStringList>
 #include <QSlider>
 #include <QTableWidget>
+#include <QImage>
+#include <QPixmap>
 #include <QFileInfo>
 
 
@@ -96,16 +98,21 @@ void MyMusicPlayer::showSongs()
         ui->infoSong->setItem(row,0,new QTableWidgetItem(query.value(1).toString()));
         ui->infoSong->setItem(row,1,new QTableWidgetItem(query.value(2).toString()));
         ui->infoSong->setItem(row,2,new QTableWidgetItem(query.value(3).toString()));
-        ui->infoSong->setItem(row,3,new QTableWidgetItem(query.value(4).toString()));
-        playerList->addMedia(QUrl(query.value(0).toString()));
+        //query.value()
+        int m = query.value(4).toInt()/1000/60;
+        int s = query.value(4).toInt()/1000%60;
+        char *times;
+        sprintf(times,"%.2d:%.2d",m,s);
+        ui->infoSong->setItem(row,3,new QTableWidgetItem(QString(times)));
+        playerList->addMedia(QUrl("file:///"+query.value(0).toString()));
+//        qDebug() << "initial=====================" <<endl;
+//        qDebug() << QUrl("file:///"+query.value(0).toString()) << endl;
 
-        //取出当前行的内容
-//        qDebug() << query.value(0).toString()
-//                 << query.value(1)
-//                 << query.value(2)
-//                 << query.value(3)
-//                 << query.value(4).toInt();
+
     }
+    ui->infoSong->selectRow(0);//设置当前光标所在行
+    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(0, 0)->text()));//初始化现在播放的歌曲名字
+     ui->lbding->setText("/"+ui->infoSong->item(0, 3)->text());//初始化时间
 
 }
 
@@ -166,7 +173,7 @@ void MyMusicPlayer::addFile()
         ui->infoSong->setItem(row,1,new QTableWidgetItem(author));
         ui->infoSong->setItem(row,2,new QTableWidgetItem(albumTitle));
         ui->infoSong->setItem(row,3,new QTableWidgetItem(QString::number(duration)));
-        playerList->addMedia(QUrl(path));   //如果可以加入数据库再进行添加到播放列表
+        playerList->addMedia(QUrl(filepath));   //如果可以加入数据库再进行添加到播放列表
     }
 
 
@@ -205,6 +212,12 @@ void MyMusicPlayer::prevSong()
     }
     playerList->setCurrentIndex(currentIndex);
     player->play();
+    ui->infoSong->selectRow(currentIndex);
+    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(currentIndex, 0)->text()));//设置现在播放的歌曲名字
+    //修改歌曲的总时间
+
+    ui->lbding->setText(ui->infoSong->item(currentIndex, 3)->text());
+
 }
 
 void MyMusicPlayer::nextSong()
@@ -213,7 +226,14 @@ void MyMusicPlayer::nextSong()
     if(++currentIndex==playerList->mediaCount()) currentIndex=0;
     playerList->setCurrentIndex(currentIndex);
     player->play();
+    ui->infoSong->selectRow(currentIndex);
+    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(currentIndex, 0)->text()));//设置现在播放的歌曲名字
+
+    //修改歌曲的总时间
+
+    ui->lbding->setText("/"+ui->infoSong->item(currentIndex, 3)->text());
 }
+
 
 void MyMusicPlayer::isVolume()
 {
@@ -268,10 +288,306 @@ void MyMusicPlayer::sliderChange(int position)
 void MyMusicPlayer::changeSliderProcess(qint64 duration)
 {
     ui->slProcess->setValue(duration);
+
+    int m = duration/1000/60;
+    //static int m = 0;
+    int s = duration/1000%60;
+
+    //qDebug() << m << s;
+    char times[6];
+    sprintf(times,"%.2d:%.2d",m,s);
+    ui->lanow->setText(QString(times));
 }
 
 void MyMusicPlayer::setSliderMaxnumber(qint64 duration)
 {
     ui->slProcess->setMaximum(duration);
 }
+
+//#include "mymusicplayer.h"
+//#include "ui_mymusicplayer.h"
+//#include <QTextCodec>
+//#include <QFile>
+//#include <QMessageBox>
+//#include <QFileDialog>
+//#include <QDebug>
+//#include <QStringList>
+//#include <QSlider>
+//#include <QTableWidget>
+//#include <QFileInfo>
+
+
+//MyMusicPlayer::MyMusicPlayer(QWidget *parent) :
+//    QMainWindow(parent),
+//    ui(new Ui::MyMusicPlayer)
+//{
+//    QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));//设置中文不乱码
+
+
+
+//    //================end============================================
+
+
+
+//    isPlay = false;//初始化不播放
+//    isvolume = true;//初始化不静音
+
+//    player = new QMediaPlayer(this);
+//    playerList = new QMediaPlaylist;
+//    playerList->setPlaybackMode(QMediaPlaylist::Loop);
+
+//    ui->setupUi(this);
+//    player->setPlaylist(playerList);
+//    //player->play();
+
+
+//    connect(ui->psPuse,&QPushButton::clicked,this,&MyMusicPlayer::pauseStart);//播放暂停按钮
+//    connect(ui->psAddfile,&QPushButton::clicked,this,&MyMusicPlayer::addFile);//添加文件按钮
+//    connect(ui->pbPrev,&QPushButton::clicked,this,&MyMusicPlayer::prevSong);//上一曲按钮事件
+//    connect(ui->psNext,&QPushButton::clicked,this,&MyMusicPlayer::nextSong);//下一曲按钮事件
+//    connect(ui->psVolume,&QPushButton::clicked,this,&MyMusicPlayer::isVolume);//声音按钮静音和开放
+//    connect(ui->psPlayType,&QPushButton::clicked,this,&MyMusicPlayer::changePlayType);//播放模式的更换
+//    connect(ui->slProcess,&QSlider::sliderMoved,this,&MyMusicPlayer::sliderChange);//slider的手动滑动改变歌曲播放的进度
+//    connect(player,&QMediaPlayer::durationChanged,this,&MyMusicPlayer::setSliderMaxnumber);//取得歌曲总时长，并设置slider的取值范围
+//    connect(player,&QMediaPlayer::positionChanged,this,&MyMusicPlayer::changeSliderProcess);//当歌曲的当前s改变，修改slider的值
+//           //void durationChanged(qint64 duration)
+
+
+
+//    //=============tablewidget初始化相关操作======================
+//    QStringList header;
+//    ui->infoSong->setRowCount(0);
+//    ui->infoSong->setColumnCount(4); //设置列数为5
+//    qDebug() << "here";
+//    header << "歌曲名" << "艺术家" << "唱片集" << "时长";
+
+//    ui->infoSong->setHorizontalHeaderLabels(header);
+
+
+
+//    showSongs();
+//}
+
+//MyMusicPlayer::~MyMusicPlayer()
+//{
+//    delete ui;
+//    delete player;
+//    delete playerList;
+//    db.close();
+
+//}
+
+//void MyMusicPlayer::showSongs()
+//{
+
+
+//    //==============数据库相关操作======================================
+//    db = QSqlDatabase::addDatabase("QSQLITE");
+//    //设置数据库
+//    db.setDatabaseName("C:/Users/Administrator/Desktop/myproject/MusicPlayer/MusicPlayer/info.db");
+//    //打开数据库
+//    if(!db.open())
+//    {
+//        //QMessageBox::information(this,QString::fromLocal8Bit("错误"),db.lastError(),QMessageBox::Ok);
+//        return ;
+//    }
+//    QSqlQuery query;
+//    //query.exec("create table musicInfo(songname varchar(20),singer varchar(20)),album varchar(10),durantion int);");//创建表格
+//    query.exec("select * from musicInfo");
+//    while(query.next())//一行一行的遍历
+//    {
+//        int row = ui->infoSong->rowCount();
+
+//        ui->infoSong->insertRow(row);
+
+//        ui->infoSong->setItem(row,0,new QTableWidgetItem(query.value(1).toString()));
+//        ui->infoSong->setItem(row,1,new QTableWidgetItem(query.value(2).toString()));
+//        ui->infoSong->setItem(row,2,new QTableWidgetItem(query.value(3).toString()));
+//        ui->infoSong->setItem(row,3,new QTableWidgetItem(query.value(4).toString()));
+//        playerList->addMedia(QUrl(query.value(0).toString()));
+
+//        //取出当前行的内容
+////        qDebug() << query.value(0).toString()
+////                 << query.value(1)
+////                 << query.value(2)
+////                 << query.value(3)
+////                 << query.value(4).toInt();
+//    }
+//    ui->infoSong->selectRow(0);//设置当前光标所在行
+//    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(0, 0)->text()));//初始化现在播放的歌曲名字
+
+//}
+
+//void MyMusicPlayer::pauseStart()
+//{
+//    if(playerList->isEmpty())
+//    {
+//        QMessageBox::information(this,"温馨提示","播放列表没有歌曲,请先添加歌曲！",QMessageBox::Ok);
+//        return;
+//    }
+//    if(isPlay == false)
+//    {
+//        player->play();
+//        ui->psPuse->setStyleSheet("border-image: url(:/img/pause.png);");
+//        isPlay = true;
+//    }
+//    else{
+//        player->pause();
+//         ui->psPuse->setStyleSheet("border-image: url(:/img/play.png);");
+//         isPlay = false;
+//    }
+//}
+
+//void MyMusicPlayer::addFile()
+//{
+//    QFile file(this);
+//    QString path = QFileDialog::getOpenFileName(this,"open","../","TXT(*.mp3 *.flac *.ape *.wav)");
+//    QString filepath;
+//    filepath = "file:///"+path;
+//    QMediaPlayer tempPlayer;
+//    tempPlayer.setMedia(QUrl(filepath));
+
+//    qDebug() << filepath << endl;
+
+
+
+//    //元数据的解析需要时间，所以这里需要循环等待（但同时需要保持Qt事件处理机制在运行）
+//    while(!tempPlayer.isMetaDataAvailable()){
+//        QCoreApplication::processEvents();
+//    }
+
+//    QStringList list=tempPlayer.availableMetaData();//调试时查看有哪些元数据可用
+
+//    if(tempPlayer.isMetaDataAvailable()){
+//        //歌曲信息
+//        author = tempPlayer.metaData(QStringLiteral("Author")).toStringList().join(",");
+
+//        title = tempPlayer.metaData(QStringLiteral("Title")).toString();
+//        albumTitle = tempPlayer.metaData(QStringLiteral("AlbumTitle")).toString();
+//        audioBitRate = tempPlayer.metaData(QStringLiteral("AudioBitRate")).toInt();
+//        duration=tempPlayer.duration();
+
+//        int row = ui->infoSong->rowCount();
+
+//        ui->infoSong->insertRow(row);
+
+//        ui->infoSong->setItem(row,0,new QTableWidgetItem(title));
+//        ui->infoSong->setItem(row,1,new QTableWidgetItem(author));
+//        ui->infoSong->setItem(row,2,new QTableWidgetItem(albumTitle));
+//        ui->infoSong->setItem(row,3,new QTableWidgetItem(QString::number(duration)));
+//        playerList->addMedia(QUrl(path));   //如果可以加入数据库再进行添加到播放列表
+//    }
+
+
+//    //打开数据库
+//    if(!db.open())
+//    {
+//        //QMessageBox::information(this,QString::fromLocal8Bit("错误"),db.lastError(),QMessageBox::Ok);
+//        qDebug() << "de open is error" << endl;
+//        return ;
+//    }
+
+//    QSqlQuery query;
+//    QString a = "woshishei";
+//    QString order = QString("insert into musicInfo values ('%1','%2','%3','%4',%5);").arg(path,title,author,albumTitle, QString::number(duration));
+//    qDebug() << "order is" << endl;
+//    qDebug() << order  << endl;
+//    qDebug() << a << endl;
+//    if(query.exec(order) == false)
+//    {
+//        qDebug() << "default" << endl;
+//    }
+
+//    qDebug() << "this is song info:" << endl;
+//    qDebug() << path << endl;
+//    qDebug() << author << title <<
+//    albumTitle<<  audioBitRate    << duration;
+
+//}
+
+//void MyMusicPlayer::prevSong()
+//{
+//    int currentIndex=playerList->currentIndex();
+//    if(--currentIndex<0)
+//    {
+//        currentIndex=playerList->mediaCount()-1;
+//    }
+//    playerList->setCurrentIndex(currentIndex);
+
+//    ui->infoSong->selectRow(currentIndex);
+//    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(currentIndex, 0)->text()));//设置现在播放的歌曲名字
+
+//    player->play();
+//}
+
+//void MyMusicPlayer::nextSong()
+//{
+//    int currentIndex=playerList->currentIndex();
+//    if(++currentIndex==playerList->mediaCount()) currentIndex=0;
+//    playerList->setCurrentIndex(currentIndex);
+//    player->play();
+
+//    ui->infoSong->selectRow(currentIndex);
+//    ui->showNowSong->setText(tr("正在播放: %1").arg(ui->infoSong->item(currentIndex, 0)->text()));//设置现在播放的歌曲名字
+//}
+
+//void MyMusicPlayer::isVolume()
+//{
+//    if(isvolume == false)//静音的时候按下
+//    {
+//        player->setVolume(voulumeNow);
+//        ui->psVolume->setStyleSheet("border-image: url(:/img/soundsize.png);");
+//        isvolume = true;
+//    }
+//    else//没有静音的时候按下
+//    {
+//        voulumeNow = player->volume();
+//        player->setVolume(0);
+//        ui->psVolume->setStyleSheet("border-image: url(:/img/mute.png);");
+//        isvolume = false;
+//    }
+//}
+
+//void MyMusicPlayer::changePlayType()
+//{
+//    //qDebug() << playerList->playbackMode();
+//    switch (playerList->playbackMode()) {
+//    case QMediaPlaylist::CurrentItemInLoop:
+//    {
+//        playerList->setPlaybackMode(QMediaPlaylist::Loop);
+//        ui->psPlayType->setStyleSheet("border-image: url(:/img/loop.png);");
+
+//        break;
+//    }
+//    case QMediaPlaylist::Loop:
+//    {
+//        playerList->setPlaybackMode(QMediaPlaylist::Random);
+//        ui->psPlayType->setStyleSheet("border-image: url(:/img/random.png);");
+//        break;
+//    }
+//    case QMediaPlaylist::Random:
+//    {
+//        playerList->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+//        ui->psPlayType->setStyleSheet("border-image: url(:/img/singleloop.png);");
+//        break;
+//    }
+//    default:
+//        break;
+//    }
+//}
+
+//void MyMusicPlayer::sliderChange(int position)
+//{
+//    player->setPosition(position);
+//}
+
+//void MyMusicPlayer::changeSliderProcess(qint64 duration)
+//{
+//    ui->slProcess->setValue(duration);
+//}
+
+//void MyMusicPlayer::setSliderMaxnumber(qint64 duration)
+//{
+//    ui->slProcess->setMaximum(duration);
+//}
 
--- build-MusicPlayer-Desktop_Qt_5_9_0_MinGW_32bit-Debug/ui_mymusicplayer.h
+++ build-MusicPlayer-Desktop_Qt_5_9_0_MinGW_32bit-Debug/ui_mymusicplayer.h
@@ -15,11 +15,14 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSlider>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTableWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -29,23 +32,36 @@ class Ui_MyMusicPlayer
 public:
     QAction *actionaddFile;
     QWidget *centralWidget;
-    QPushButton *psPuse;
+    QVBoxLayout *verticalLayout_2;
+    QWidget *widget_5;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer_2;
+    QLabel *showNowSong;
+    QSpacerItem *horizontalSpacer_3;
+    QTableWidget *infoSong;
+    QWidget *widget_2;
+    QVBoxLayout *verticalLayout;
+    QSlider *slProcess;
+    QWidget *widget_4;
+    QHBoxLayout *horizontalLayout_3;
+    QSpacerItem *horizontalSpacer;
+    QLabel *lanow;
+    QLabel *lbding;
+    QWidget *widget_3;
+    QHBoxLayout *horizontalLayout_2;
     QPushButton *psAddfile;
+    QPushButton *psPlayType;
     QPushButton *pbPrev;
+    QPushButton *psPuse;
     QPushButton *psNext;
     QPushButton *psVolume;
-    QWidget *widget;
-    QHBoxLayout *horizontalLayout;
-    QTableWidget *infoSong;
-    QPushButton *psPlayType;
-    QSlider *slProcess;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MyMusicPlayer)
     {
         if (MyMusicPlayer->objectName().isEmpty())
             MyMusicPlayer->setObjectName(QStringLiteral("MyMusicPlayer"));
-        MyMusicPlayer->resize(888, 519);
+        MyMusicPlayer->resize(956, 557);
         MyMusicPlayer->setStyleSheet(QLatin1String("#centralWidget{\n"
 "border-image: url(:/img/backimg.jpg);\n"
 "}"));
@@ -53,60 +69,134 @@ public:
         actionaddFile->setObjectName(QStringLiteral("actionaddFile"));
         centralWidget = new QWidget(MyMusicPlayer);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        psPuse = new QPushButton(centralWidget);
-        psPuse->setObjectName(QStringLiteral("psPuse"));
-        psPuse->setGeometry(QRect(410, 390, 50, 50));
-        psPuse->setMinimumSize(QSize(50, 50));
-        psPuse->setMaximumSize(QSize(50, 50));
-        psPuse->setStyleSheet(QStringLiteral("border-image: url(:/img/play.png);"));
-        psAddfile = new QPushButton(centralWidget);
-        psAddfile->setObjectName(QStringLiteral("psAddfile"));
-        psAddfile->setGeometry(QRect(130, 390, 50, 50));
-        psAddfile->setMinimumSize(QSize(50, 50));
-        psAddfile->setMaximumSize(QSize(50, 50));
-        psAddfile->setStyleSheet(QStringLiteral("border-image: url(:/img/file.png);"));
-        pbPrev = new QPushButton(centralWidget);
-        pbPrev->setObjectName(QStringLiteral("pbPrev"));
-        pbPrev->setGeometry(QRect(330, 390, 50, 50));
-        pbPrev->setMinimumSize(QSize(50, 50));
-        pbPrev->setMaximumSize(QSize(50, 50));
-        pbPrev->setStyleSheet(QStringLiteral("border-image: url(:/img/prev.png);"));
-        psNext = new QPushButton(centralWidget);
-        psNext->setObjectName(QStringLiteral("psNext"));
-        psNext->setGeometry(QRect(500, 390, 50, 50));
-        psNext->setMinimumSize(QSize(50, 50));
-        psNext->setMaximumSize(QSize(50, 50));
-        psNext->setStyleSheet(QStringLiteral("border-image: url(:/img/next.png);"));
-        psVolume = new QPushButton(centralWidget);
-        psVolume->setObjectName(QStringLiteral("psVolume"));
-        psVolume->setGeometry(QRect(700, 400, 50, 50));
-        psVolume->setMinimumSize(QSize(50, 50));
-        psVolume->setMaximumSize(QSize(50, 50));
-        psVolume->setStyleSheet(QStringLiteral("border-image: url(:/img/soundsize.png);"));
-        widget = new QWidget(centralWidget);
-        widget->setObjectName(QStringLiteral("widget"));
-        widget->setGeometry(QRect(160, 70, 641, 281));
-        horizontalLayout = new QHBoxLayout(widget);
+        verticalLayout_2 = new QVBoxLayout(centralWidget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        widget_5 = new QWidget(centralWidget);
+        widget_5->setObjectName(QStringLiteral("widget_5"));
+        horizontalLayout = new QHBoxLayout(widget_5);
         horizontalLayout->setSpacing(6);
         horizontalLayout->setContentsMargins(11, 11, 11, 11);
         horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
-        infoSong = new QTableWidget(widget);
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_2);
+
+        showNowSong = new QLabel(widget_5);
+        showNowSong->setObjectName(QStringLiteral("showNowSong"));
+
+        horizontalLayout->addWidget(showNowSong);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_3);
+
+
+        verticalLayout_2->addWidget(widget_5);
+
+        infoSong = new QTableWidget(centralWidget);
         infoSong->setObjectName(QStringLiteral("infoSong"));
         infoSong->setStyleSheet(QStringLiteral("background-color: rgb(0, 0, 0, 0);"));
 
-        horizontalLayout->addWidget(infoSong);
+        verticalLayout_2->addWidget(infoSong);
 
-        psPlayType = new QPushButton(centralWidget);
+        widget_2 = new QWidget(centralWidget);
+        widget_2->setObjectName(QStringLiteral("widget_2"));
+        verticalLayout = new QVBoxLayout(widget_2);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        slProcess = new QSlider(widget_2);
+        slProcess->setObjectName(QStringLiteral("slProcess"));
+        slProcess->setMaximum(100);
+        slProcess->setOrientation(Qt::Horizontal);
+
+        verticalLayout->addWidget(slProcess);
+
+        widget_4 = new QWidget(widget_2);
+        widget_4->setObjectName(QStringLiteral("widget_4"));
+        horizontalLayout_3 = new QHBoxLayout(widget_4);
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer);
+
+        lanow = new QLabel(widget_4);
+        lanow->setObjectName(QStringLiteral("lanow"));
+
+        horizontalLayout_3->addWidget(lanow);
+
+        lbding = new QLabel(widget_4);
+        lbding->setObjectName(QStringLiteral("lbding"));
+
+        horizontalLayout_3->addWidget(lbding);
+
+
+        verticalLayout->addWidget(widget_4);
+
+
+        verticalLayout_2->addWidget(widget_2);
+
+        widget_3 = new QWidget(centralWidget);
+        widget_3->setObjectName(QStringLiteral("widget_3"));
+        horizontalLayout_2 = new QHBoxLayout(widget_3);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        psAddfile = new QPushButton(widget_3);
+        psAddfile->setObjectName(QStringLiteral("psAddfile"));
+        psAddfile->setMinimumSize(QSize(50, 50));
+        psAddfile->setMaximumSize(QSize(50, 50));
+        psAddfile->setStyleSheet(QStringLiteral("border-image: url(:/img/file.png);"));
+
+        horizontalLayout_2->addWidget(psAddfile);
+
+        psPlayType = new QPushButton(widget_3);
         psPlayType->setObjectName(QStringLiteral("psPlayType"));
-        psPlayType->setGeometry(QRect(40, 300, 50, 50));
         psPlayType->setMinimumSize(QSize(50, 50));
         psPlayType->setMaximumSize(QSize(50, 50));
         psPlayType->setStyleSheet(QStringLiteral("border-image: url(:/img/loop.png);"));
-        slProcess = new QSlider(centralWidget);
-        slProcess->setObjectName(QStringLiteral("slProcess"));
-        slProcess->setGeometry(QRect(89, 360, 761, 22));
-        slProcess->setMaximum(100);
-        slProcess->setOrientation(Qt::Horizontal);
+
+        horizontalLayout_2->addWidget(psPlayType);
+
+        pbPrev = new QPushButton(widget_3);
+        pbPrev->setObjectName(QStringLiteral("pbPrev"));
+        pbPrev->setMinimumSize(QSize(50, 50));
+        pbPrev->setMaximumSize(QSize(50, 50));
+        pbPrev->setStyleSheet(QStringLiteral("border-image: url(:/img/prev.png);"));
+
+        horizontalLayout_2->addWidget(pbPrev);
+
+        psPuse = new QPushButton(widget_3);
+        psPuse->setObjectName(QStringLiteral("psPuse"));
+        psPuse->setMinimumSize(QSize(50, 50));
+        psPuse->setMaximumSize(QSize(50, 50));
+        psPuse->setStyleSheet(QStringLiteral("border-image: url(:/img/play.png);"));
+
+        horizontalLayout_2->addWidget(psPuse);
+
+        psNext = new QPushButton(widget_3);
+        psNext->setObjectName(QStringLiteral("psNext"));
+        psNext->setMinimumSize(QSize(50, 50));
+        psNext->setMaximumSize(QSize(50, 50));
+        psNext->setStyleSheet(QStringLiteral("border-image: url(:/img/next.png);"));
+
+        horizontalLayout_2->addWidget(psNext);
+
+        psVolume = new QPushButton(widget_3);
+        psVolume->setObjectName(QStringLiteral("psVolume"));
+        psVolume->setMinimumSize(QSize(50, 50));
+        psVolume->setMaximumSize(QSize(50, 50));
+        psVolume->setStyleSheet(QStringLiteral("border-image: url(:/img/soundsize.png);"));
+
+        horizontalLayout_2->addWidget(psVolume);
+
+
+        verticalLayout_2->addWidget(widget_3);
+
         MyMusicPlayer->setCentralWidget(centralWidget);
         statusBar = new QStatusBar(MyMusicPlayer);
         statusBar->setObjectName(QStringLiteral("statusBar"));
@@ -121,12 +211,15 @@ public:
     {
         MyMusicPlayer->setWindowTitle(QApplication::translate("MyMusicPlayer", "MyMusicPlayer", Q_NULLPTR));
         actionaddFile->setText(QApplication::translate("MyMusicPlayer", "\346\267\273\345\212\240\346\226\207\344\273\266", Q_NULLPTR));
-        psPuse->setText(QString());
+        showNowSong->setText(QApplication::translate("MyMusicPlayer", "TextLabel", Q_NULLPTR));
+        lanow->setText(QApplication::translate("MyMusicPlayer", "TextLabel", Q_NULLPTR));
+        lbding->setText(QApplication::translate("MyMusicPlayer", "TextLabel", Q_NULLPTR));
         psAddfile->setText(QString());
+        psPlayType->setText(QString());
         pbPrev->setText(QString());
+        psPuse->setText(QString());
         psNext->setText(QString());
         psVolume->setText(QString());
-        psPlayType->setText(QString());
     } // retranslateUi
 
 };
